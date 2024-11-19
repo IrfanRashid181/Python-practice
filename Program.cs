1c98aa1f7a3ab36182deeb008b8b11f937330a92
Irfan Rashid <52149947+IrfanRashid181@users.noreply.github.com>
2024-11-19 17:20:18 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassandObjects1
+{
+    internal class Program
+    {
+        class Car
+        {
+            public int id;
+            public string name;
+            public void Drive()
+
+            {
+                Console.WriteLine($"{id} car is {name}");
+            }
+                }
+        static void Main(string[] args)
+        {
+              Car car = new Car();
+            car.id = 112;
+            car.name  = "Test";
+            car.Drive();
+
+        }
+    }
+}
